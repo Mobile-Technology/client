1556af43c8a2d6f5f7fd86d4b998ac8337a56051
Spyder-code <luaysyauqillah@gmail.com>
2021-04-13 11:54:56 +0700

    login

---
+++ src/components/Paragraph.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native-paper'
+import { theme } from '../core/theme'
+
+export default function Paragraph(props) {
+  return <Text style={styles.text} {...props} />
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25,
+    lineHeight: 21,
+    textAlign: 'center',
+    marginBottom: 12,
+    fontFamily: 'Game On_PersonalUseOnly',
+    fontWeight:'bold',
+    color: theme.colors.dark,
+  },
+})
---
+++ src/core/theme.js
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#560CCE',
+    secondary: '#414757',
+    error: '#f13a59',
+    gold: '#DAA520',
+    white: '#D3D3D3',
+    dark: '#2F4F4F',
+  },
+}
